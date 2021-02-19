054da299bdfacda79c7d8a9a230c1cf7b2f645b3
Twip322 <samprp458804@gmail.com>
2021-02-19 08:48:34 +0400

    PatternAdded

---
+++ EmployeeManagement3/Patterns/IBuilderUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EM_3_MainLogic.Patterns
+{
+    public interface IBuilderUnit
+    {
+        void setName();
+        void setSurname();
+        void setPatronymic();
+        void setSkills();
+        void setWorkExp();
+        EM_3_MainLogic.Binding_Model.Employee Build();
+    }
+}
